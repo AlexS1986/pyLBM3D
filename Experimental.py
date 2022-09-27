Sign in sigma corrected

Experimental.py
@@ -86,7 +86,7 @@ def sigma(secondMomentArg,dtArg,secondSourceArg):
     for i in range(0, len(sigmaOut)):
         for j in range(0, len(sigmaOut[0])):
             for k in range(0, len(sigmaOut[0][0])):
-                sigmaOut[i,j,k] = secondMomentArg[i,j,k] + dtArg/2.0 * secondSourceArg[i,j,k]
+                sigmaOut[i,j,k] = -secondMomentArg[i,j,k] - dtArg/2.0 * secondSourceArg[i,j,k]
     return sigmaOut
 
 
@@ -179,7 +179,7 @@ def computeSigmaBd(sigmaBC, sigmaArg, fArg, ccArg, cArg, uArg, dxArg, laArg, mue
                                 sigmaBd[i,j,l,ii,jj] = sigmaBC[ii,jj]
         return sigmaBd
 
-    def computeSigmaBdWithoutExtrapolation(sigmaArg, ccArg, coordinateArg,coordinateValueArg):
+    def computeSigmaBdWithoutExtrapolation(sigmaArg, ccArg, coordinateArg, coordinateValueArg):
         sigmaAtCoordinate = BC.selectAtCoordinate(sigmaArg,coordinateArg,coordinateValueArg)
         sigmaBd = np.zeros((len(sigmaArg),len(sigmaArg[0]),len(ccArg),3,3), dtype=np.double)
 

MainExperimental.py
@@ -48,6 +48,7 @@ b = np.zeros((maxX,maxY,maxZ,3), dtype=np.double)
 tMax = 2.0
 t = 0.0
 k = int(0)
+
 while(t <= tMax):
     fNew = np.zeros((maxX, maxY, maxZ, 27), dtype=np.double)
     fNew.fill(np.nan)
@@ -55,8 +56,8 @@ while(t <= tMax):
     rho = Core.computeRho(f)
     jOld = j
     j = Ex.j(Core.firstMoment(f, cc), dt, Ex.firstSource(b, rho0))
-    sigma = Ex.sigma(Core.secondMoment(f,cc), dt, Ex.secondSource(Util.computeDivergenceUFromDisplacementField(j, dx), lam, mue, rho0))
-    u = Core.computeU(u, rho, j, jOld, dt, rho0)
+    sigma = Ex.sigma(Core.secondMoment(f, cc), dt, Ex .secondSource(Util.computeDivergenceUFromDisplacementField(j, dx), lam, mue, rho0))
+    u = Ex.computeU(u, rho0, j, jOld, dt)
 
     PostProcessing.writeVTKMaster(k, nameOfSimulation, pathToVTK, t, xx, u)
 
@@ -85,6 +86,7 @@ while(t <= tMax):
                         [np.nan, np.nan, 0],
                         [0.0, 0.0, 0.0]])
     sigmaBCZMax = sigmaBCZMin
+
 
     #edges
     sigmaBCEdgeXY = np.array([[sigmaXX, 0.0, 0.0],